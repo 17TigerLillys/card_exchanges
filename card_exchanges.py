14dabd541ba0285355d44ef576185e1e408749cc
Juliana Richardson <julie@Julianas-MacBook-Air.local>
2024-05-07 16:03:36 -0400

    add two args and update file paths accordingly

--- card_exchanges.py
+++ card_exchanges.py
@@ -43,38 +43,42 @@ def determineLocation(city, state, zipCode, intAddress, envName):
 parser = argparse.ArgumentParser(description='This program cleans and processes survey data for card exchanges')
 parser.add_argument('-t','--type', help='Options are petsParents or allAlums', required=True)
 parser.add_argument('-s','--stage', help='Number 1-4 corresponding to the desired stage', required=True)
+parser.add_argument('-new','--newData', help='File path of the data', required=True)
+parser.add_argument('-old','--oldData', help='Optional file path of the old data for Stage 3', required=False)
 args = vars(parser.parse_args())
 
-catsDF = pd.read_excel('./swlc_cleanish_data copy.xlsx', sheet_name=None) # New Data
-# print(catsDF.columns)
-dogsDF = pd.read_excel('./swld_rough_data.xlsx', sheet_name=None) # Old Data
-	# if (catsDF["Email"] != catsDF["Email Address"]) == True:
-	# 	print(catsDF["Email"], catsDF["Email Address"])
-	# print(catsDF[catsDF['Email'] != catsDF['Email Address']]['Email'], catsDF[catsDF['Email'] != catsDF['Email Address']]['Email Address'])
+
+newDF = pd.read_excel(args['newData'], sheet_name=None) 
+# print(newDF.columns)
+if args['stage'] == '3':
+	oldDF = pd.read_excel(args['oldData'], sheet_name=None)
+	# if (newDF["Email"] != newDF["Email Address"]) == True:
+	# 	print(newDF["Email"], newDF["Email Address"])
+	# print(newDF[newDF['Email'] != newDF['Email Address']]['Email'], newDF[newDF['Email'] != newDF['Email Address']]['Email Address'])
 	# df[df['uid'] == query]
 
 # Gut check to show any different emails
 #---------------------------------------
 # This should be a lambda function!!!!!!!!!!!!!
-# for index, row in catsDF.iterrows():
+# for index, row in newDF.iterrows():
 # 	if row['Email'] != row['Email Address']:
 # 		print(row['Email Address'], '\t', row['Email'])
 
 
 
-if args['stage'] == '1':
+if args['stage'] == '1' or args['stage'] == 2:
 	# Fix the messed up zip codes
 	#----------------------------
-	catsDF['cleanZip'] = catsDF.apply(lambda x: cleanZips(x.Zip_Code, x.State), axis=1)
+	newDF['cleanZip'] = newDF.apply(lambda x: cleanZips(x.Zip_Code, x.State), axis=1)
 
 
 	# Create a new column for location and those that need to be manually fixed 
-	catsDF['location'] = catsDF.apply(lambda x: determineLocation(x.City, x.State, x.cleanZip, x.international_address, x.envelope_name), axis=1)
+	newDF['location'] = newDF.apply(lambda x: determineLocation(x.City, x.State, x.cleanZip, x.international_address, x.envelope_name), axis=1)
 
 if args['stage'] == '2':
 	# Check for duplicates
 	# First check for duplicates by email address
-	catEmailDupes = catsDF[catsDF.duplicated('Email Address', keep=False)]
+	catEmailDupes = newDF[newDF.duplicated('Email Address', keep=False)]
 	if not catEmailDupes.empty:
 		print('You may have duplicates by email, check!')
 		print(catEmailDupes['Email Address'])
@@ -82,14 +86,14 @@ if args['stage'] == '2':
 	# Next check by zip code to catch anyone who signed up with a different email address
 	# to-do: Try fuzzy string matching on address line 1!!!!!
 	#		Internationals will need to use international_address
-	catZipDupes = catsDF[catsDF.duplicated('cleanZip', keep=False)].sort_values(['Zip_Code', 'envelope_name'])
+	catZipDupes = newDF[newDF.duplicated('cleanZip', keep=False)].sort_values(['Zip_Code', 'envelope_name'])
 	if not catZipDupes.empty:
 		print('You may have duplicates by zip code, check!')
 		print(catZipDupes[['envelope_name', 'cleanZip']])
 
 if args['stage'] == '3':
 	# Check new emails against the emails from last year
-	print(set(dogsDF['Email Address']) - set(catsDF['Email Address'])) 
+	print(set(oldDF['Email Address']) - set(newDF['Email Address'])) 
 
 
 # Different cases to check for
@@ -97,8 +101,8 @@ if args['stage'] == '3':
 
 
 
-# print(catsDF[['location', 'international_address']])
-# for index, row in catsDF.iterrows():
+# print(newDF[['location', 'international_address']])
+# for index, row in newDF.iterrows():
 # 	if row['location'] == 'international':
 # 		print(row['location'], '\t', row['international_address'])
 
